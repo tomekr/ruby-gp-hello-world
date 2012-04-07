4edb0e63e004f5ec25542a20fc15d1280bdcdcf4
Tomek Rabczak <tomek.rabczak@gmail.com>
2012-04-07 15:39:03 -0500

    init_population working

--- hello_world/genetic_hello.rb
+++ hello_world/genetic_hello.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'ostruct'
+require 'pp'
 
 MAX_ITERATIONS = 16384
 POPULATION_SIZE = 2048
@@ -7,6 +8,22 @@ ELITISM_RATE = 0.10
 MUTATION_RATE = 0.25
 GA_TARGET = "Hello world!"
 
+Struct.new "GAStruct", :str, :fitness
+
+def init_population
+  population  = []
+  buffer      = []
+  target_size = GA_TARGET.size
+
+  (0..POPULATION_SIZE).each do
+    citizen = Struct::GAStruct.new "", 0 
+
+    (0..target_size).each { citizen.str << (rand(90) + 32).chr }
+    population << citizen
+  end
+
+  return [population, buffer]
+end
 
 def calc_fitness(population)
   #TODO
@@ -18,7 +35,6 @@ end
 
 def print_best(population)
   #TODO
-
 end
 
 def mate(population, buffer)
@@ -31,10 +47,8 @@ end
 
 # Main Program
 
-pop_alpha, pop_beta = init_population
-
-population = pop_alpha
-buffer     = pop_beta
+population, buffer = init_population
+pp population
 
 (0..MAX_ITERATIONS).each do
   calc_fitness    population
